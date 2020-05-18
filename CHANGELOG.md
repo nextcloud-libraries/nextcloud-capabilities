1afb54cdda8564a0ffcb887f922dad97098806f2
Roeland Jago Douma <roeland@famdouma.nl>
2020-05-18 16:27:27 +0200

    Initial commit
    Basic support for obtaining the capabilities
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## 1.2.3 - 2020-04-06
+### Changed
+- Dependency updates
+### Fixed
+- Update vulnerable packages
+
+## 1.2.2 - 2020-03-19
+### Changed
+- Dependency updates
+### Fixed
+- Update vulnerable packages
+
+## 1.2.1 - 2020-01-10
+### Fixed
+- Bug in @nextcloud/event-bus dependency
+
+## 1.2.0 - 2020-01-07
+### Changed
+- Updated dependencies
---
+++ lib/index.ts
@@ -0,0 +1,13 @@
+import { loadState } from '@nextcloud/initial-state'
+
+export function getCapabilities(): Object {
+	try {
+		return loadState('core', 'capabilities')
+	} catch (error) {
+        console.debug('Could not find capabilities initial state fall back to _oc_capabilities')
+        if (!('_oc_capabilities' in window)) {
+            throw Error('Could not locate capabilities')
+        }
+        return window['_oc_capabilities']
+	}
+}
