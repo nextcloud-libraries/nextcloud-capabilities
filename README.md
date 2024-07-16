fdfe2eda3b423cf85e35cc47885b935a033d5d8b
Andy Scherzinger <info@andy-scherzinger.de>
2024-07-16 18:17:25 +0200

    chore: Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+<!--
+  - SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: GPL-3.0-or-later
+-->
 # @nextcloud/capabilities
 
 Nextcloud helper to obtain the server capabilities in apps.
--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 import { loadState } from '@nextcloud/initial-state'
 
 export function getCapabilities(): Object {
