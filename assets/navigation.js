71fc61ae537efb2593a62aa63edc87c3d139999b
susnux <susnux@users.noreply.github.com>
2025-11-07 09:50:56 +0000

    deploy: e0c129322cd00450bde9fc79f8a8bd0b776d6c03

--- assets/navigation.js
+++ assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJKTy1xTixITMrMySzJTC1W0lEqSCzJULJSSivNSy7JzM8r1kdTopdRkpujpKOUnZmXomRlZlIbCwD4YBWPTgAAAA=="
+window.navigationData = "eJyLrlYqSa0oUbJSSk8tcU4sSEzKzMksyUwtVtJRKkgsyVCyUkorzUsuyczPK9ZHU6KXUZKbo6SjlJ2Zl6JkZWZSGwsAS1EbjA=="
